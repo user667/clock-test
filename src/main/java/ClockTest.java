5f3d24ac947681a1375f3dd36bbc5366e10bbe76
user667 <philip.mundt@magnolia-cms.com>
2016-04-02 02:07:56 +0200

    Add clock test

---
+++ src/main/java/ClockTest.java
@@ -0,0 +1,31 @@
+import java.util.Calendar;
+
+/**
+ * Clock test.
+ */
+public class ClockTest {
+    static final int HAD = 60 * 24;
+    static final int[] NUM = new int[]{
+            /* 0 */ 6, /* 1 */ 2, /* 2 */ 5, /* 3 */ 5, /* 4 */ 4,
+            /* 5 */ 5, /* 6 */ 6, /* 7 */ 3, /* 8 */ 7, /* 9 */ 6,
+    };
+    static int getSum(final int hour, final int min) {
+        return NUM[hour % 10] + NUM[hour / 10] + NUM[min % 10] + NUM[min / 10];
+    }
+    public static void main(String[] args) {
+        final Calendar calendar = Calendar.getInstance();
+        int max = 0;
+        String maxStr = "";
+        for (int i = 0; i < HAD; i++) {
+            calendar.add(Calendar.MINUTE, 1);
+            final int hour = calendar.get(Calendar.HOUR);
+            final int min = calendar.get(Calendar.MINUTE);
+            final int sum = getSum(hour, min);
+            if (sum > max) {
+                max = sum;
+                maxStr = String.format("%02d:%02d%n", hour, min);
+            }
+        }
+        System.out.print(maxStr);
+    }
+}
---
+++ src/main/java/ClockTestDbg.java
@@ -0,0 +1,35 @@
+import java.util.Calendar;
+
+/**
+ * Clock test.
+ */
+public class ClockTestDbg extends ClockTest {
+    private static final boolean DBG = true;
+    private static final int KB = 1024;
+    public static void main(String[] args) {
+        final Calendar calendar = Calendar.getInstance();
+        final long start = calendar.getTimeInMillis();
+        int max = 0;
+        String maxStr = "";
+        for (int i = 0; i < HAD; i++) {
+            calendar.add(Calendar.MINUTE, 1);
+            final int hour = calendar.get(Calendar.HOUR);
+            final int min = calendar.get(Calendar.MINUTE);
+            final String time = String.format("%02d:%02d%n", hour, min);
+            int sum = getSum(hour, min);
+            if (sum > max) {
+                max = sum;
+                maxStr = time;
+            }
+            System.out.print(DBG ? time : "");
+        }
+        System.out.printf(DBG ? "-----%n" : "");
+        System.out.print(maxStr);
+        System.out.printf("%dms%n", System.currentTimeMillis() - start);
+
+        final Runtime runtime = Runtime.getRuntime(); // Get the Java runtime
+        runtime.gc(); // Run the garbage collector
+        long memory = runtime.totalMemory() - runtime.freeMemory(); // Calculate the used memory
+        System.out.printf("%dkb%n", memory / KB); // Used memory
+    }
+}
