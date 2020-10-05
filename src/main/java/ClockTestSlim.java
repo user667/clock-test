92129671aaf0115899f23f81e076734fcd4aee9f
Philip Mundt <philip.mundt@sympany.ch>
2020-10-05 15:56:46 +0200

    Further reduce line count

--- src/main/java/ClockTestSlim.java
+++ src/main/java/ClockTestSlim.java
@@ -1,17 +1,11 @@
 public class ClockTestSlim {
-    protected static final int[] NUM = new int[] {
-            /* 0 */ 6, /* 1 */ 2, /* 2 */ 5, /* 3 */ 5, /* 4 */ 4,
-            /* 5 */ 5, /* 6 */ 6, /* 7 */ 3, /* 8 */ 7, /* 9 */ 6,
-    };
-    protected static int getSum(final int hour, final int min) {
-        return NUM[hour % 10] + NUM[hour / 10] + NUM[min % 10] + NUM[min / 10];
-    }
+    private static final int[] NUMBER_OF_LIT_SEGMENTS = new int[]{/* 0 */ 6, /* 1 */ 2, /* 2 */ 5, /* 3 */ 5, /* 4 */ 4,/* 5 */ 5, /* 6 */ 6, /* 7 */ 3, /* 8 */ 7, /* 9 */ 6};
     public static void main(String[] args) {
         int max = 0;
         String maxStr = "";
         for (int hour = 0; hour < 24; hour++) {
             for (int minute = 0; minute < 60; minute++) {
-                final int sum = getSum(hour, minute);
+                final int sum = NUMBER_OF_LIT_SEGMENTS[hour % 10] + NUMBER_OF_LIT_SEGMENTS[hour / 10] + NUMBER_OF_LIT_SEGMENTS[minute % 10] + NUMBER_OF_LIT_SEGMENTS[minute / 10];
                 if (sum > max) {
                     max = sum;
                     maxStr = String.format("%02d:%02d%n", hour, minute);
